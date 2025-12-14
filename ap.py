dcde1b3d7bd50f913699cdbae42be576340d24a7
Ahmad878-code <affan.grewal73@gmail.com>
2025-12-14 13:17:17 +0500

    ap

---
+++ ap.py
@@ -0,0 +1,11 @@
+# input a word
+text = str(input("Enter a word:"))
+
+#Reverse string
+# using step values as -1 to iterate in reverse
+revText = text[::-1]
+text = revText
+
+print("Reverse of given string is:")
+print(text)
+
